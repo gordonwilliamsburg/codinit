cf3e0b6864874e2e64206832e19ff1e718ee7923
gordonwilliamsburg <zarroukinesrine@gmail.com>
2023-11-26 15:38:38 +0100

    add description of args in docstring for codebase queries

--- backend/src/codinit/queries.py
+++ backend/src/codinit/queries.py
@@ -3,8 +3,12 @@ from typing import List
 from codinit.config import client
 
 
-def get_files(prompt: str, k=5):
-    """Returns code file relevant for a given prompt"""
+def get_files(prompt: str, k: int = 1):
+    """Returns code file relevant for a given prompt
+    Args:
+        prompt: str, description of the file to search for.
+        k: int, the number of most similar files to the prompt to be returned by the query.
+    """
     result = (
         client.query.get(
             "File",
@@ -22,8 +26,12 @@ def get_files(prompt: str, k=5):
     return result["data"]["Get"]["File"]
 
 
-def get_classes(prompt: str, k=5):
-    """Returns code classes relevant for a given prompt"""
+def get_classes(prompt: str, k: int = 1):
+    """Returns code classes relevant for a given prompt
+    Args:
+        prompt: str, description of the class to search for.
+        k: int, the number of most similar classes to the prompt to be returned by the query.
+    """
     result = (
         client.query.get(
             "Class",
@@ -36,8 +44,12 @@ def get_classes(prompt: str, k=5):
     return result["data"]["Get"]["Class"]
 
 
-def get_imports(prompt: str, k=5):
-    """Returns code imports relevant for a given prompt"""
+def get_imports(prompt: str, k: int = 1):
+    """Returns code imports relevant for a given prompt
+    Args:
+        prompt: str, description of the import to search for.
+        k: int, the number of most similar imports to the prompt to be returned by the query.
+    """
     result = (
         client.query.get(
             "Import",
@@ -50,7 +62,33 @@ def get_imports(prompt: str, k=5):
     return result["data"]["Get"]["Import"]
 
 
-def get_exact_imports(query: str, k=5):
+def get_functions(prompt: str, k: int = 1):
+    """Returns code functions relevant for a given prompt
+    Args:
+        prompt: str, description of the function to search for.
+        k: int, the number of most similar functions to the prompt to be returned by the query.
+    """
+    result = (
+        client.query.get(
+            "Function",
+            properties=[
+                "name",
+                "code",
+                "parameters",
+                "variables",
+                "return_value",
+                "belongsToFile {... on File {name}}",
+                "belongsToClass {... on Class {name}}",
+            ],
+        )
+        .with_near_text({"concepts": [prompt]})
+        .with_limit(k)
+        .do()
+    )
+    return result["data"]["Get"]["Function"]
+
+
+def get_exact_imports(query: str, k: int = 1):
     """Returns exact imports relevant for a given prompt"""
     result = (
         client.query.get(
@@ -70,25 +108,3 @@ def get_imports_from_kg(import_list: List[str], library_name: str, k=10):
         exists_in_weaviate_kg = get_exact_imports(query=import_name, k=k)
         result[import_name] = exists_in_weaviate_kg
     return result
-
-
-def get_functions(prompt: str, k=5):
-    """Returns code functions relevant for a given prompt"""
-    result = (
-        client.query.get(
-            "Function",
-            properties=[
-                "name",
-                "code",
-                "parameters",
-                "variables",
-                "return_value",
-                "belongsToFile {... on File {name}}",
-                "belongsToClass {... on Class {name}}",
-            ],
-        )
-        .with_near_text({"concepts": [prompt]})
-        .with_limit(k)
-        .do()
-    )
-    return result["data"]["Get"]["Function"]
