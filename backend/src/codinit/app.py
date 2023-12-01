fbebc05289ded17ea8e0110f97cf296ed1382654
gordonwilliamsburg <zarroukinesrine@gmail.com>
2023-12-01 14:40:26 +0100

    fix app to work with current tasexecutor pipeline

--- backend/src/codinit/app.py
+++ backend/src/codinit/app.py
@@ -3,10 +3,12 @@ from typing import List
 
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
 from pydantic import BaseModel
 
 from codinit.code_editor import PythonCodeEditor
 from codinit.config import client
+from codinit.get_context_ import get_relevant_documents
 from codinit.queries import get_functions
 from codinit.task_executor import TaskExecutionConfig, TaskExecutor
 
@@ -54,25 +56,47 @@ async def generate(websocket: WebSocket):
         task = item.prompt
         # source_code=item.source_code
         task_executor = TaskExecutor(code_editor, config)
-        relevant_docs = task_executor.get_docs(task=task)  # libraries=libraries,
-        plan = task_executor.planner.execute_task(task=task, context=relevant_docs)
+        retriever = WeaviateHybridSearchRetriever(
+            client=client,
+            index_name="DocumentionFile",
+            text_key="content",
+            k=5,
+            alpha=0.75,
+        )
+        relevant_docs = get_relevant_documents(query=task, retriever=retriever)
+        plan = task_executor.planner.execute(
+            tool_choice="execute_plan",
+            chat_history=[],
+            task=task,
+            context=relevant_docs,
+        )[0]
         await websocket.send_json({"plan": "\n".join(plan), "code": ""})
         # install dependencies from plan
         if (
             task_executor.config.execute_code
             and task_executor.config.install_dependencies
         ):
-            deps = task_executor.dependency_tracker.execute_task(plan="\n".join(plan))
+            deps = task_executor.dependency_tracker.execute(
+                tool_choice="install_dependencies", chat_history=[], plan=plan
+            )[0]
             await websocket.send_json(
                 {"plan": "", "code": "", "error": f"dependencies to install: {deps}"}
             )
             task_executor.install_dependencies(deps)
+        chat_history = []
+        chat_history.append(
+            {"role": "assistant", "content": f"installed dependencies {deps}"}
+        )
         # generate code
-        new_code = task_executor.coder.execute_task(
-            source_code=task_executor.code_editor.display_code(),
-            objective=task,
-            plan="\n".join(plan),
+        new_code = task_executor.coder.execute(
+            task=task,
+            tool_choice="execute_code",
+            chat_history=chat_history,
+            plan=plan,
             context=relevant_docs,
+        )[0]
+        new_code, _, _ = task_executor.format_lint_code(
+            code=new_code, dependencies=deps
         )
         error = task_executor.run_code(new_code)
         await websocket.send_json(
@@ -86,8 +110,16 @@ async def generate(websocket: WebSocket):
             if attempt > task_executor.config.coding_attempts:
                 break
             # corrected code
-            new_code = task_executor.code_corrector.execute_task(
-                context=relevant_docs, source_code=new_code, error=error
+            new_code = task_executor.code_corrector.execute(
+                tool_choice="execute_code",
+                chat_history=[],
+                task=task,
+                context=relevant_docs,
+                source_code=new_code,
+                error=error,
+            )[0]
+            new_code, _, _ = task_executor.format_lint_code(
+                code=new_code, dependencies=deps
             )
             await websocket.send_json(
                 {"plan": "\n".join(plan), "code": new_code, "error": error}
