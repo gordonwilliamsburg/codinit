df42589ae2c7a7f7f0884600aa767aed0c0248db
gordonwilliamsburg <zarroukinesrine@gmail.com>
2023-12-26 22:38:09 +0100

    CODY-90 save scraped data to json

---
+++ backend/src/codinit/documentation/save_data.py
@@ -0,0 +1,36 @@
+import json
+from typing import List
+
+from pydantic import HttpUrl
+
+from codinit.documentation.pydantic_models import WebScrapingData
+
+
+def save_scraped_data_as_json(data: List[WebScrapingData], filename: str):
+    """
+    saves data in json file, overwrites the file.
+    TODO check if URL has been already visited and only save new data, or check for changes..
+    Better to be done with some DB.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json_data = [
+            {"url": item.url, "text": item.text, "metadata": item.metadata.dict()}
+            for item in data
+        ]
+        json.dump(json_data, file, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    from apify_client import ApifyClient
+
+    from codinit.config import secrets
+    from codinit.documentation.apify_webscraper import WebScraper
+
+    client = ApifyClient(secrets.apify_key)
+    scraper = WebScraper(client)
+    urls = [HttpUrl(url="https://docs.apify.com/academy/web-scraping-for-beginners")]
+    scraped_data_models = scraper.run_scraping(urls=urls)
+    for model in scraped_data_models:
+        print(model.text)
+    filename = "apify_test.json"
+    save_scraped_data_as_json(scraped_data_models, secrets.docs_dir + filename)
