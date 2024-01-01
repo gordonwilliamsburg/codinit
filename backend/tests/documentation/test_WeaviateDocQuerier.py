7e68db607688f6147789baa1878f5489e9705846
gordonwilliamsburg <zarroukinesrine@gmail.com>
2024-01-01 05:46:18 +0100

    CODY-92 test chunk_doc method of WeaviateDocLoader

--- backend/tests/documentation/test_WeaviateDocQuerier.py
+++ backend/tests/documentation/test_WeaviateDocQuerier.py
@@ -40,15 +40,19 @@ def test_weaviate_doc_loader_initialization(mock_client, mock_documentation_sett
 
 
 def test_get_raw_documentation_file_exists(mock_library, mock_client, mock_documentation_settings, mock_secrets, sample_data):
-    # Mock os.path.exists to simulate the file exists
+    """
+    verifies that the file existence check, file reading, and data loading are called with the correct arguments,
+    and that the method returns the correct data.
+    """
+    # Mock os.path.exists to simulate the file exists function, simulates the presence of the JSON file.
     mock_exists = Mock(return_value=True)
     os.path.exists = mock_exists
 
-    # Mock the load_scraped_data_from_json function
+    # Mock the load_scraped_data_from_json function, simulates the loading of data from the JSON file.
     mock_load_json = Mock(return_value=sample_data)
     WeaviateDocLoader.load_json = mock_load_json
 
-    # Mock file open
+    # mock the file open and read operations.
     mock_file_open = mock_open(read_data=json.dumps([data.dict() for data in sample_data]))
     with patch('builtins.open', mock_file_open):
         # Initialize WeaviateDocLoader
@@ -70,3 +74,28 @@ def test_get_raw_documentation_file_exists(mock_library, mock_client, mock_docum
 
         # Assert that the returned data matches the sample data
         assert result == sample_data
+
+def test_chunk_doc(mock_library, mock_client, mock_documentation_settings, mock_secrets, sample_data):
+    # Initialize WeaviateDocLoader
+    doc_loader = WeaviateDocLoader(
+        library=mock_library,
+        client=mock_client,
+        documentation_settings=mock_documentation_settings,
+        secrets=mock_secrets
+    )
+
+    # Mock the chunk_document function
+    with patch('codinit.documentation.get_context.chunk_document', return_value=["chunk1", "chunk2"]) as mock_chunk_document:
+        sample_web_scraping_data = sample_data[0]
+        # Call chunk_doc
+        result = doc_loader.chunk_doc(doc=sample_web_scraping_data)
+
+        # Check if chunk_document was called with correct parameters
+        mock_chunk_document.assert_called_once_with(
+            document=sample_web_scraping_data.text,
+            chunk_size=mock_documentation_settings.chunk_size,
+            overlap=mock_documentation_settings.overlap
+        )
+
+        # Assert that the returned chunks match the mock return value
+        assert result == ["chunk1", "chunk2"]
