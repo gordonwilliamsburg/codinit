f374f1d19cceae4cff87cad3af46711377e8e673
gordonwilliamsburg <zarroukinesrine@gmail.com>
2024-02-22 11:42:06 +0100

    CODY-105 update doc schema to weaviate v4

--- backend/src/codinit/documentation/doc_schema.py
+++ backend/src/codinit/documentation/doc_schema.py
@@ -1,73 +1,86 @@
-library_class = {
-    "class": "Library",
-    "description": "A code library",
-    "vectorizer": "text2vec-openai",
-    "moduleConfig": {
-        "text2vec-openai": {"model": "ada", "modelVersion": "002", "type": "text"}
-    },
-    "properties": [
-        {"name": "name", "dataType": ["text"]},
-        {"name": "links", "dataType": ["text[]"]},
-        {"name": "description", "dataType": ["text"]},
-        {
-            "name": "hasDocumentationFile",
-            "dataType": ["DocumentationFile"],
-            "description": "Documentation of the library",
-        },
-    ],
-}
+import weaviate
+import weaviate.classes.config as wvcc
 
-documentation_file_class = {
-    "class": "DocumentationFile",
-    "description": "A documentation file of a library",
-    "vectorizer": "text2vec-openai",
-    "moduleConfig": {
-        "text2vec-openai": {"model": "ada", "modelVersion": "002", "type": "text"}
-    },
-    "properties": [
-        {
-            "name": "title",
-            "dataType": ["text"],
-            "description": "title of the document",
-        },
-        {
-            "name": "description",
-            "dataType": ["text"],
-            "description": "Description of the content of the documentation file",
-        },
-        {
-            "name": "chunknumber",
-            "dataType": ["int"],
-            "description": "Order of the chunk in the original documentation file",
-        },
-        {
-            "name": "source",
-            "dataType": ["text"],
-            "description": "URL source of the documentation file",
-        },
-        {
-            "name": "language",
-            "dataType": ["text"],
-            "description": "Language of the documentation file",
-        },
-        {
-            "name": "content",
-            "dataType": ["text"],
-            "description": "Content of the documentation file",
-        },
-        {
-            "name": "fromLibrary",
-            "dataType": ["Library"],
-            "description": "The library the documentation belongs to",
-        },
-    ],
-}
 
+def create_documentation_schema(client: weaviate.WeaviateClient):
+    try:
+        # Create the Library collection
+        library_collection = client.collections.create(
+            name="Library",
+            vectorizer_config=wvcc.Configure.Vectorizer.text2vec_openai(
+                model="ada", model_version="002"
+            ),
+            properties=[
+                wvcc.Property(name="name", data_type=wvcc.DataType.TEXT),
+                wvcc.Property(name="links", data_type=wvcc.DataType.TEXT_ARRAY),
+                wvcc.Property(name="description", data_type=wvcc.DataType.TEXT),
+            ],
+        )
+
+        # Create the DocumentationFile collection
+        documentation_file_collection = client.collections.create(
+            name="DocumentationFile",
+            vectorizer_config=wvcc.Configure.Vectorizer.text2vec_openai(
+                model="ada", model_version="002"
+            ),
+            properties=[
+                wvcc.Property(
+                    name="title",
+                    data_type=wvcc.DataType.TEXT,
+                    description="title of the document",
+                ),
+                wvcc.Property(
+                    name="description",
+                    data_type=wvcc.DataType.TEXT,
+                    description="Description of the content of the documentation file",
+                ),
+                wvcc.Property(
+                    name="chunknumber",
+                    data_type=wvcc.DataType.INT,
+                    description="Order of the chunk in the original documentation file",
+                ),
+                wvcc.Property(
+                    name="source",
+                    data_type=wvcc.DataType.TEXT,
+                    description="URL source of the documentation file",
+                ),
+                wvcc.Property(
+                    name="language",
+                    data_type=wvcc.DataType.TEXT,
+                    description="Language of the documentation file",
+                ),
+                wvcc.Property(
+                    name="content",
+                    data_type=wvcc.DataType.TEXT,
+                    description="Content of the documentation file",
+                ),
+            ],
+        )
+
+        library_collection.config.add_reference(
+            ref=wvcc.ReferenceProperty(
+                name="hasDocumentationFile",
+                target_collection="DocumentationFile",
+                description="Documentation of the library",
+            )
+        )
+
+        documentation_file_collection.config.add_reference(
+            ref=wvcc.ReferenceProperty(
+                name="fromLibrary",
+                target_collection="Library",
+                description="The library the documentation belongs to",
+            )
+        )
+    finally:
+        client.close()
+    return library_collection, documentation_file_collection
+
+
+# Create the schema
 if __name__ == "__main__":
     from codinit.weaviate_client import get_weaviate_client
 
     client = get_weaviate_client()
-
-    client.schema.delete_class("Library")
-    client.schema.delete_class("DocumentationFile")
-    # client.schema.create({"classes": [library_class, documentation_file_class]})
+    client.collections.delete_all()
+    library_class, documentation_file_class = create_documentation_schema(client)
